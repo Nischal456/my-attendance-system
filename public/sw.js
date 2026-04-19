d0c511d44de3fcbf13c189b2b6711bdb461c321d
Nischal456 <shresthanischal456@gmail.com>
2026-04-19 16:39:20 +0545

    new

--- public/sw.js
+++ public/sw.js
@@ -4,8 +4,8 @@ self.addEventListener('push', function (event) {
       const data = event.data.json();
       const options = {
         body: data.body,
-        icon: data.icon || '/paw.png',
-        badge: '/paw.png',
+        icon: data.icon || '/favicon.ico',
+        badge: '/favicon.ico',
         vibrate: [100, 50, 100],
         data: {
           dateOfArrival: Date.now(),
